c2fbd3ae84f46aef7d870f512f49f3455c1fa0d4
Martin Ankerl <martin.ankerl@gmail.com>
2019-12-30 20:45:32 +0100

    compiles

--- cmake/Misc.cmake
+++ cmake/Misc.cmake
@@ -4,10 +4,11 @@
 # Have CMake parse the config file, generating the config header, with
 # correct definitions. Here only used to make version number available to
 # the source code. Include "exampleConfig.h" (no .in suffix) in the source.
-configure_file (
-  "${PROJECT_SOURCE_DIR}/include/exampleConfig.h.in"
-  "${PROJECT_BINARY_DIR}/exampleConfig.h"
-)
+
+#configure_file (
+#  "${PROJECT_SOURCE_DIR}/include/exampleConfig.h.in"
+#  "${PROJECT_BINARY_DIR}/exampleConfig.h"
+#)
 # add the binary tree to the search path for include files
 # so that we will find exampleConfig.h
 include_directories("${PROJECT_BINARY_DIR}")
---
+++ cmake/Nanobench.cmake
@@ -0,0 +1,2 @@
+add_library(nanobench INTERFACE)
+target_include_directories(nanobench INTERFACE ${PROJECT_SOURCE_DIR}/external/nanobench/src/include)
