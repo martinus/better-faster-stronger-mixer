9e2173809d3743e2eca8bc7ea572da1c992b2b95
Martin Ankerl <martin.ankerl@gmail.com>
2020-01-06 12:07:10 +0100

    rotr and b

--- include/mixer/mumxmumxx2.h
+++ include/mixer/mumxmumxx2.h
@@ -13,6 +13,12 @@ inline uint64_t mumxmumxx2(uint64_t v) {
 }
 
 inline uint64_t mumxmumxx_work_in_progress(uint64_t v) {
-    return mumx(mumx(v, UINT64_C(0xa0761d6478bd642f)),
-                (v ^ (v >> 27)) * UINT64_C(0x3C79AC492BA7B653));
+    static constexpr auto a = UINT64_C(0xa0761d6478bd642f);
+    static constexpr auto b = UINT64_C(0xe7037ed1a0b428db);
+
+    // 3.25 v ^ rotr(v, 25) ^ rotr(v, 47) ^ b
+    // 3.17 v ^ rotr(v, 25) ^ rotr(v ^ b, 47)
+    // 3.20 v ^ rotr(v ^ b, 25) ^ rotr(v, 47)
+    // 3.25 v ^ b ^ rotr(v, 25) ^ rotr(v, 47)
+    return mumx(mumx(v, a), v ^ rotr(v, 25) ^ rotr(v ^ b, 47));
 }
